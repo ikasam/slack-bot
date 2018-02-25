81e1901b17633727aa316d0a5d5e0e25b93fc151
ikasam <ikasam.k503@gmail.com>
2018-02-26 01:47:58 +0900

    Delete env SLACK_BOT_USERNAME and Add HTML escaping of KEYWORD.

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ $ cp .env.sample .env
 2. Write environment variable on `.env`
 ```
 SLACK_BOT_TOKEN=xoxp-XXXXXXXXXXX-XXXXXXXXXXXX-XXXXXXXXXXXXX-XXXXXXXXXXXXXX
-SLACK_BOT_USERNAME=botuser
 SLACK_CHANNEL=#general
 SLACK_CHANNEL_OPS=#bot-test
 KEYWORD=hoge
--- slack-bot.rb
+++ slack-bot.rb
@@ -1,11 +1,12 @@
 require 'bundler/setup'
 require 'slack-ruby-client'
+require 'cgi/escape'
+require 'pp'
 
 TOKEN        = ENV['SLACK_BOT_TOKEN']
 CHANNEL      = ENV['SLACK_CHANNEL'] || '#general'
 CHANNEL_OPS  = ENV['SLACK_CHANNEL_OPS']
-BOT_USERNAME = ENV['SLACK_BOT_USERNAME']
-KEYWORD      = ENV['SLACK_KEYWORD']
+KEYWORD      = CGI.escapeHTML(ENV['SLACK_KEYWORD'])
 
 Slack.configure do |conf|
   conf.token = TOKEN
@@ -15,21 +16,24 @@ end
 client = Slack::Web::Client.new
 CHANNEL_ID     = client.channels_info(channel: CHANNEL).channel.id
 CHANNEL_OPS_ID = client.channels_info(channel: CHANNEL_OPS).channel.id unless CHANNEL_OPS.nil?
-BOT_USER_ID    = client.users_info(user: BOT_USERNAME).user.id unless BOT_USERNAME.nil?
 
 # RTM Connection
 client = Slack::RealTime::Client.new
 
 client.on :hello do
+  BOT_USER_ID    = client.self.id
   puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
   client.message channel: CHANNEL_OPS_ID, text: "RTM Connected: keyword=#{KEYWORD} in <##{CHANNEL_ID}>" unless CHANNEL_OPS_ID.nil? or KEYWORD.nil?
 end
 
 client.on :message do |data|
   if (not KEYWORD.empty?) and data.channel == CHANNEL_ID
+    pp data
     case data.text
     when /#{KEYWORD}/ then
       client.message channel: data.channel, text: "<!channel> yo!"
+    when /#{BOT_USER_ID}/ then
+      client.message channel: data.channel, text: "<@#{data.user}> Hi."
     end
   end
 end
