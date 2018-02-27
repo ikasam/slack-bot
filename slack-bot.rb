407222d96625eb2f5e6688f3ba20beda7e3ff29c
ikasam <ikasam.k503@gmail.com>
2018-02-27 23:12:50 +0900

    Change chat post API and export message to env.

--- slack-bot.rb
+++ slack-bot.rb
@@ -4,43 +4,44 @@ require 'slack-ruby-client'
 TOKEN        = ENV['SLACK_BOT_TOKEN']
 CHANNEL      = ENV['SLACK_CHANNEL'] || '#general'
 CHANNEL_OPS  = ENV['SLACK_CHANNEL_OPS']
-INTERVAL     = (ENV['INTERVAL'] || 15).to_i
+MESSAGE      = (ENV['SLACK_MESSAGE'] || 'yo!').gsub(/\\n/,"\n")
 
 Slack.configure do |conf|
   conf.token = TOKEN
 end
 
 # Get IDs via Web API
-client = Slack::Web::Client.new
-CHANNEL_ID     = client.channels_info(channel: CHANNEL).channel.id
-CHANNEL_OPS_ID = client.channels_info(channel: CHANNEL_OPS).channel.id unless CHANNEL_OPS.nil?
+web_client = Slack::Web::Client.new
+CHANNEL_ID     = web_client.channels_info(channel: CHANNEL).channel.id
+CHANNEL_OPS_ID = web_client.channels_info(channel: CHANNEL_OPS).channel.id unless CHANNEL_OPS.nil?
 
 # RTM Connection
-client = Slack::RealTime::Client.new
+rtm_client = Slack::RealTime::Client.new
 
-client.on :hello do
-  BOT_USER_ID    = client.self.id
-  puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
-  client.message channel: CHANNEL_OPS_ID, text: "RTM Connected: interval=#{INTERVAL} in <##{CHANNEL_ID}>" unless CHANNEL_OPS_ID.nil? or INTERVAL.nil?
+rtm_client.on :hello do
+  BOT_USER_ID    = rtm_client.self.id
+  puts "Successfully connected, welcome '#{rtm_client.self.name}' to the '#{rtm_client.team.name}' team at https://#{rtm_client.team.domain}.slack.com."
+  web_client.chat_postMessage channel: CHANNEL_OPS_ID, text: "RTM Connected: in <##{CHANNEL_ID}>", as_user: false unless CHANNEL_OPS_ID.nil?
 end
 
-client.on :message do |data|
+rtm_client.on :message do |data|
   if data.channel == CHANNEL_ID
-    m = /\d+年\d+月\d+日/.match(data.text)
-    reserve_day = Date.strptime(m[0], "%Y年%m月%d日")
-    date_diff = (reserve_day - Date.today).to_i
-    if date_diff <= INTERVAL
-      client.message channel: data.channel, text: "<!channel> #{INTERVAL}日以内の予約です!"
+    if (m = /\d+年\d+月\d+日/.match(data.text))
+      reserve_day = Date.strptime(m[0], "%Y年%m月%d日")
+      date_diff = (reserve_day - Date.today).to_i
+      if date_diff <= 1
+        web_client.chat_postMessage channel: data.channel, text: "<!channel> \n" + MESSAGE, as_user: false
+      end
     end
   end
 end
 
-client.on :close do |_data|
+rtm_client.on :close do |_data|
   puts "Client is about to disconnect"
 end
 
-client.on :closed do |_data|
+rtm_client.on :closed do |_data|
   puts "Client has disconnected successfully!"
 end
 
-client.start!
+rtm_client.start!
