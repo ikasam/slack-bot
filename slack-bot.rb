ac81f51a2b622c319e6ddccba848c0c99ce318b9
ikasam <ikasam.k503@gmail.com>
2018-03-04 03:43:21 +0900

    Now the bot revive when RTM client has disconnected.

--- slack-bot.rb
+++ slack-bot.rb
@@ -12,36 +12,41 @@ Slack.configure do |conf|
 end
 
 # Get IDs via Web API
-client = Slack::Web::Client.new
-CHANNEL_ID     = client.channels_info(channel: CHANNEL).channel.id
-CHANNEL_OPS_ID = client.channels_info(channel: CHANNEL_OPS).channel.id unless CHANNEL_OPS.nil?
+WEB_CLIENT = Slack::Web::Client.new
+CHANNEL_ID     = WEB_CLIENT.channels_info(channel: CHANNEL).channel.id
+CHANNEL_OPS_ID = WEB_CLIENT.channels_info(channel: CHANNEL_OPS).channel.id unless CHANNEL_OPS.nil?
 
-# RTM Connection
-client = Slack::RealTime::Client.new
-
-client.on :hello do
-  BOT_USER_ID    = client.self.id
-  puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
-  client.message channel: CHANNEL_OPS_ID, text: "RTM Connected: keyword=#{KEYWORD} in <##{CHANNEL_ID}>" unless CHANNEL_OPS_ID.nil? or KEYWORD.nil?
-end
-
-client.on :message do |data|
-  if (not KEYWORD.empty?) and data.channel == CHANNEL_ID
-    case data.text
-    when /#{KEYWORD}/ then
-      client.message channel: data.channel, text: "<!channel> yo!"
-    when /#{BOT_USER_ID}/ then
-      client.message channel: data.channel, text: "<@#{data.user}> Hi."
+def start!
+  # RTM Connection
+  client = Slack::RealTime::Client.new
+  
+  client.on :hello do
+    @bot_user_id = client.self.id
+    puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
+    WEB_CLIENT.chat_postMessage channel: CHANNEL_OPS_ID, text: "RTM Connected: keyword=#{KEYWORD} in <##{CHANNEL_ID}>", as_user: false unless CHANNEL_OPS_ID.nil? or KEYWORD.nil?
+  end
+  
+  client.on :message do |data|
+    if (not KEYWORD.empty?) and data.channel == CHANNEL_ID
+      case data.text
+      when /#{KEYWORD}/ then
+        WEB_CLIENT.chat_postMessage channel: data.channel, text: "<!channel> yo!", as_user: false
+      when /#{@bot_user_id}/ then
+        WEB_CLIENT.chat_postMessage channel: data.channel, text: "<@#{data.user}> Hi.", as_user: false
+      end
     end
   end
+  
+  client.on :close do |_data|
+    puts "Client is about to disconnect"
+  end
+  
+  client.on :closed do |_data|
+    puts "Client has disconnected successfully!"
+    start!
+  end
+
+  client.start!
 end
 
-client.on :close do |_data|
-  puts "Client is about to disconnect"
-end
-
-client.on :closed do |_data|
-  puts "Client has disconnected successfully!"
-end
-
-client.start!
+start!
